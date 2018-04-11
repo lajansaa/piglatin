734b8bd69a43036eb9708e691e348c273193bf6d
Isa <lajansa@hotmail.com>
2018-04-11 10:19:18 +0800

    submission (isa)

--- word.rb
+++ word.rb
@@ -1,1 +1,25 @@
 # write your solution here
+def pig_latin(input)
+  vowels = ["a", "e", "i", "o", "u"]
+  if vowels.include? input[0]
+    return input + "way"
+  else
+    # loop through words until vowel is reached
+    count = 0
+    prev_char = ''
+    input.each_char.with_index do |letter, index|
+      if vowels.include?(letter) || (index > 0 && letter == 'y')
+        if (prev_char == 'q' && letter == 'u')
+          count += 1
+        end
+        break
+      else
+        prev_char = letter
+        count += 1
+      end
+    end
+    return input.slice(count, input.length) + input.slice(0, count) + "ay"
+  end
+end
+
+puts pig_latin("squeal")
